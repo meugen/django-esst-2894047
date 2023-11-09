5c3b9ee531ad92eb66e5bc9cc4fdf4d10ea3b6c8
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2023-11-09 23:50:11 +0200

    Use class-based views for notes

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.list),
-    path('notes/<int:pk>', views.detail),
+    path('notes', views.NodesListView.as_view()),
+    path('notes/<int:pk>', views.NodesDetailView.as_view()),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NodesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
 
-
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk = pk)
-    except Notes.DoesNotExist:
-        raise Http404('Note doesn\'t exist')
-    return render(request, 'notes/notes_detail.html', {'note': note})
+class NodesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
