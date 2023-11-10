1812e23a70bbb5ca1001f3ddb32806db4adfc9d8
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2023-11-10 17:15:53 +0200

    Update notes

--- notes/forms.py
+++ notes/forms.py
@@ -7,6 +7,13 @@ class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
         fields = ('title', 'text')
+        labels = {
+            'text': 'Write your thoughts here:'
+        }
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control my-5'}),
+            'text': forms.Textarea(attrs={'class': 'form-control mb-5'})
+        }
 
     def clean_title(self):
         title = self.cleaned_data['title']
--- notes/urls.py
+++ notes/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('notes', views.NodesListView.as_view(), name='notes.list'),
     path('notes/<int:pk>', views.NodesDetailView.as_view(), name='notes.detail'),
+    path('notes/<int:pk>/edit', views.NotesUpdateView.as_view(), name='notes.update'),
     path('notes/new', views.NotesCreateView.as_view(), name='notes.new')
 ]
