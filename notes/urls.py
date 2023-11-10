abe04dec1dc4461ec4949f5e5469baac18646fdd
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2023-11-10 17:25:42 +0200

    Delete notes

--- notes/urls.py
+++ notes/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('notes', views.NodesListView.as_view(), name='notes.list'),
     path('notes/<int:pk>', views.NodesDetailView.as_view(), name='notes.detail'),
     path('notes/<int:pk>/edit', views.NotesUpdateView.as_view(), name='notes.update'),
+    path('notes/<int:pk>/delete', views.NotesDeleteView.as_view(), name='notes.delete'),
     path('notes/new', views.NotesCreateView.as_view(), name='notes.new')
 ]
