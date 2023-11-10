5e3eefb8db9a6f44e4d2d099f4c0a70b64ec9cbd
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2023-11-10 14:47:12 +0200

    styling

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.NodesListView.as_view()),
-    path('notes/<int:pk>', views.NodesDetailView.as_view()),
+    path('notes', views.NodesListView.as_view(), name='notes.list'),
+    path('notes/<int:pk>', views.NodesDetailView.as_view(), name='notes.detail'),
 ]
