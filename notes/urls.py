964c5d46be625e0d7a93fc75506da5d687624ade
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2023-11-10 16:13:34 +0200

    Create notes

--- notes/urls.py
+++ notes/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('notes', views.NodesListView.as_view(), name='notes.list'),
     path('notes/<int:pk>', views.NodesDetailView.as_view(), name='notes.detail'),
+    path('notes/new', views.NotesCreateView.as_view(), name='notes.new')
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import ListView
-from django.views.generic import DetailView
+from django.views.generic import CreateView, DetailView, ListView
 
 from .models import Notes
+
+class NotesCreateView(CreateView):
+    model = Notes
+    fields = ['title', 'text']
+    success_url = '/smart/notes'
 
 class NodesListView(ListView):
     model = Notes
